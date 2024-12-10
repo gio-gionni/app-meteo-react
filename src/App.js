b9279e2d6bcf5273afca19319c8707dab8dad1ab
gio-gionni <g.gazzetto@gmail.com>
2024-12-10 09:43:53 +0100

    added the git links

--- src/App.js
+++ src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project is created by{" "}
+        <a href="https://github.com/gio-gionni">Giulia</a> and it is{" "}
+        <a href="https://github.com/gio-gionni/weather-react">
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
