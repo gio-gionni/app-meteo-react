4b35aa8782236894335f2dcb5bf2a91bc7cd15c4
gio-gionni <g.gazzetto@gmail.com>
2024-12-10 10:08:00 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project is created by{" "}
-        <a href="https://github.com/gio-gionni">Giulia</a> and it is{" "}
-        <a href="https://github.com/gio-gionni/weather-react">
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project is created by{" "}
+          <a
+            href="https://github.com/gio-gionni"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Giulia
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/gio-gionni/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
